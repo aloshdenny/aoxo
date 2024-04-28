e519143356ba55a900c96e1c2f75b67cc874cdb2
Alosh Denny <97750960+aloshdenny@users.noreply.github.com>
2024-04-28 20:41:10 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -486,7 +486,7 @@ const contactPageData = {
     title: "Contact Me",
     profile_image_path: "me_circle.png",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "I'm available on almost all social media. Hit me up if you need anything :)",
   },
   blogSection: {
     title: "Blogs",
